c53e8d9b2250b875c284925148d73bf794ce5f04
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-29 19:49:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(time)
+  time = 10
+  while time > 0
+  puts "#{time} SECONDS!"
+  time -= 1
+    end
+return "HAPPY NEW YEAR!"
 end
+
+# def countdown_with_sleep(time)
+#   sleep 5
+# puts "This takes 5 seconds to execute."
+# end
